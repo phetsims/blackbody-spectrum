3dec6f2c09114767cd7aeb617cbc3f9c1492c2ce
Chris Malley <cmalley@pixelzoom.com>
2018-01-11 13:56:41 -0700

    clean up creation of tick marks #15

--- js/blackbody-spectrum/view/BlackbodySpectrumThermometer.js
+++ js/blackbody-spectrum/view/BlackbodySpectrumThermometer.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2017, University of Colorado Boulder
+// Copyright 2014-2018, University of Colorado Boulder
 
 /**
  * Scenery Node that displays a thermometer with labels attached to the right hand side of the thermometer
@@ -12,6 +12,7 @@ define( function( require ) {
   var blackbodySpectrum = require( 'BLACKBODY_SPECTRUM/blackbodySpectrum' );
   var inherit = require( 'PHET_CORE/inherit' );
   var LinearFunction = require( 'DOT/LinearFunction' );
+  var Node = require( 'SCENERY/nodes/Node' );
   var Path = require( 'SCENERY/nodes/Path' );
   var PhetFont = require( 'SCENERY_PHET/PhetFont' );
   var Shape = require( 'KITE/Shape' );
@@ -25,8 +26,12 @@ define( function( require ) {
   var sunString = require( 'string!BLACKBODY_SPECTRUM/sun' );
 
   // constants
-  var VALUE_FONT = new PhetFont( { size: 24, weight: 'bold' } );
-  var COLOR_FONT = 'yellow';
+  var TICK_MARKS = [
+    { text: sunString, temperature: 5700 },
+    { text: lightbulbString, temperature: 3000 },
+    { text: ovenString, temperature: 660 },
+    { text: earthString, temperature: 300 }
+  ];
 
   /**
    * @param {Property.<number>} temperatureProperty
@@ -34,8 +39,6 @@ define( function( require ) {
    * @constructor
    */
   function BlackbodySpectrumThermometer( temperatureProperty, options ) {
-
-    var self = this;
 
     options = _.extend( {
       minTemperature: 0,
@@ -46,68 +49,70 @@ define( function( require ) {
       glassThickness: 4,
       lineWidth: 4,
       outlineStroke: 'white',
-      tickSpacing: 15
+      tickSpacing: 15,
+      tickLabelFont: new PhetFont( { size: 24, weight: 'bold' } ),
+      tickLabelColor: 'yellow'
     }, options );
 
     ThermometerNode.call( this, options.minTemperature, options.maxTemperature, temperatureProperty, options );
 
-    // label and their associated values
-    var labels = [
-      { text: sunString, temperature: 5700 },
-      { text: lightbulbString, temperature: 3000 },
-      { text: ovenString, temperature: 660 },
-      { text: earthString, temperature: 300 }
-    ];
-
-    //TODO move this function out of the constructor
-    /**
-     * map function that relates a temperature to a height
-     * this function was reverse engineered from ThermometerNode in Scenery-Phet
-     *
-     * @returns {LinearFunction}
-     */
-    var temperatureToHeightLinearFunction = function() {
-      var fluidWidth = options.tubeWidth - options.lineWidth - options.glassThickness;
-      var clipBulbRadius = ( options.bulbDiameter - options.lineWidth - options.glassThickness ) / 2;
-      var clipStartAngle = -Math.acos( ( fluidWidth / 2 ) / clipBulbRadius );
-      var clipEndAngle = Math.PI - clipStartAngle;
-      var fluidSphereDiameter = options.bulbDiameter - options.lineWidth - options.glassThickness;
-      var fluidBottomCutoff = fluidSphereDiameter / 2 * Math.sin( clipEndAngle );
-      var height = options.tubeHeight + options.tubeWidth / 2; // need the halfcap on top
-      var maxFluidHeight = height - fluidBottomCutoff;
-      return new LinearFunction( options.minTemperature, options.maxTemperature, fluidBottomCutoff, -maxFluidHeight, true /* clamp */ );
-    };
-
-    //TODO move this function out of the constructor
-    /**
-     * Create and add text and tick label for thermometer
-     * @param {text:{string}, temperature:{number}} label
-     */
-    function labelMaker( label ) {
-      var objectHeight = temperatureToHeightLinearFunction()( label.temperature );
-      var tickMarkLength = options.tubeWidth * 0.5;
-      var shape = new Shape();
-      shape.moveTo( options.tubeWidth / 2, objectHeight ).horizontalLineToRelative( tickMarkLength );
-      var tickNode = new Path( shape, { stroke: options.outlineStroke, lineWidth: options.lineWidth } );
-      var textNode = new Text( label.text, { font: VALUE_FONT, fill: COLOR_FONT } );
-
-      self.addChild( tickNode );
-      self.addChild( textNode );
-
-      tickNode.left = tickMarkLength;
-      tickNode.centerY = objectHeight;
-      textNode.centerY = objectHeight;
-      textNode.left = tickNode.right + 10;
-    }
-
-    // runs over all the labels to be added
-    for ( var i = 0; i < labels.length; i++ ) {
-      var label = labels[ i ];
-      labelMaker( label );
+    // labeled tick marks
+    for ( var i = 0; i < TICK_MARKS.length; i++ ) {
+      this.addChild( createLabeledTick( TICK_MARKS[ i ].text, TICK_MARKS[ i ].temperature, options ) );
     }
   }
 
   blackbodySpectrum.register( 'BlackbodySpectrumThermometer', BlackbodySpectrumThermometer );
 
-  return inherit( ThermometerNode, BlackbodySpectrumThermometer );
+  inherit( ThermometerNode, BlackbodySpectrumThermometer );
+
+  /**
+   * Creates a labeled tick mark.
+   * @param {string} text
+   * @param {number} temperature
+   * @param {Object} options - options that were provided to BlackbodySpectrumThermometer constructor
+   */
+  function createLabeledTick( text, temperature, options ) {
+
+    var linearFunction = temperatureToHeightLinearFunction( options );
+    var objectHeight = linearFunction( temperature );
+    var tickMarkLength = options.tubeWidth * 0.5;
+
+    var shape = new Shape();
+    shape.moveTo( options.tubeWidth / 2, objectHeight ).horizontalLineToRelative( tickMarkLength );
+
+    var tickNode = new Path( shape, { stroke: options.outlineStroke, lineWidth: options.lineWidth } );
+    var textNode = new Text( text, { font: options.tickLabelFont, fill: options.tickLabelColor } );
+
+    var parentNode = new Node( {
+      children: [ tickNode, textNode ]
+    } );
+
+    tickNode.left = tickMarkLength;
+    tickNode.centerY = objectHeight;
+    textNode.centerY = objectHeight;
+    textNode.left = tickNode.right + 10;
+
+    return parentNode;
+  }
+
+  /**
+   * map function that relates a temperature to a height
+   * this function was reverse engineered from ThermometerNode in Scenery-Phet
+   * @param {Object} options - options that were provided to BlackbodySpectrumThermometer constructor
+   * @returns {LinearFunction}
+   */
+  var temperatureToHeightLinearFunction = function( options ) {
+    var fluidWidth = options.tubeWidth - options.lineWidth - options.glassThickness;
+    var clipBulbRadius = ( options.bulbDiameter - options.lineWidth - options.glassThickness ) / 2;
+    var clipStartAngle = -Math.acos( ( fluidWidth / 2 ) / clipBulbRadius );
+    var clipEndAngle = Math.PI - clipStartAngle;
+    var fluidSphereDiameter = options.bulbDiameter - options.lineWidth - options.glassThickness;
+    var fluidBottomCutoff = fluidSphereDiameter / 2 * Math.sin( clipEndAngle );
+    var height = options.tubeHeight + options.tubeWidth / 2; // need the halfcap on top
+    var maxFluidHeight = height - fluidBottomCutoff;
+    return new LinearFunction( options.minTemperature, options.maxTemperature, fluidBottomCutoff, -maxFluidHeight, true /* clamp */ );
+  };
+
+  return BlackbodySpectrumThermometer;
 } );
