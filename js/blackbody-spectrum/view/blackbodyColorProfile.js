a8cdbc2662c069d169c4522de4b828872cb49fdc
zepumph <michael.kauzmann@colorado.edu>
2019-07-11 12:53:55 -0800

    update copyright dates, https://github.com/phetsims/tasks/issues/988

--- js/blackbody-spectrum/view/blackbodyColorProfile.js
+++ js/blackbody-spectrum/view/blackbodyColorProfile.js
@@ -1,4 +1,4 @@
-// Copyright 2019, University of Colorado Boulder
+// Copyright 2018-2019, University of Colorado Boulder
 
 /**
  * An object that contains the colors used for various major components of the Blackbody simulation.  This
