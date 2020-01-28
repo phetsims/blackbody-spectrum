be62243563a1dce43256a2f59c220f82be1391e7
zepumph <michael.kauzmann@colorado.edu>
2020-01-27 16:58:04 -0900

    add phetioDocumentation, https://github.com/phetsims/joist/issues/591

--- js/phet-io/blackbody-spectrum-phet-io-elements-baseline.js
+++ js/phet-io/blackbody-spectrum-phet-io-elements-baseline.js
@@ -4071,7 +4071,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioTypeName": "NumberPropertyIO"
     },
     "blackbodySpectrum.general.soundManager.enabledProperty": {
-      "phetioDocumentation": "",
+      "phetioDocumentation": "If this sim contains sound, then this Property will toggle whether sound is enabled ordisabled.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
       "phetioFeatured": false,
