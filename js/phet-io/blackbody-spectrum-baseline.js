57fe4c55448cbe88c092167cd0360625b94acab9
chrisklus <christopher.klusendorf@colorado.edu>
2020-04-10 16:53:04 -0400

    Update baseline files, see https://github.com/phetsims/joist/issues/623

--- js/phet-io/blackbody-spectrum-baseline.js
+++ js/phet-io/blackbody-spectrum-baseline.js
@@ -2238,7 +2238,7 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioTypeName": "ActionIO<NumberIO, NumberIO>"
     },
     "blackbodySpectrum.general.model.screenProperty": {
-      "phetioDocumentation": "Which sim screen is selected, including the home screen",
+      "phetioDocumentation": "Determines which screen is selected in the simulation",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
       "phetioFeatured": true,
