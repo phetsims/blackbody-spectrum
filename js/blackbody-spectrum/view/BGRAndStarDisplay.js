d2e30369be4dd64748370b8bd253611e27d3d364
phet-dev <phetadmin@gmail.com>
2021-05-25 15:17:27 -0600

    update copyright dates from daily grunt work

--- js/blackbody-spectrum/view/BGRAndStarDisplay.js
+++ js/blackbody-spectrum/view/BGRAndStarDisplay.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2020, University of Colorado Boulder
+// Copyright 2019-2021, University of Colorado Boulder
 
 /**
  * Node that displays red, blue, and green color representations of the current blackbody temperature, as well as a star
