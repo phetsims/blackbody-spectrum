301a2a2b64cd6817ef26b380cc8599696ad0af64
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:40 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/blackbodySpectrumStrings.ts
@@ -0,0 +1,40 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import blackbodySpectrum from './blackbodySpectrum.js';
+
+type StringsType = {
+  'blackbody-spectrum': {
+    'title': string
+  },
+  'spectralPowerDensityLabel': string,
+  'wavelengthLabel': string,
+  'subtitleLabel': string,
+  'b': string,
+  'g': string,
+  'r': string,
+  'siriusA': string,
+  'sun': string,
+  'lightBulb': string,
+  'earth': string,
+  'blackbodyTemperature': string,
+  'kelvinUnits': string,
+  'graphValues': string,
+  'intensity': string,
+  'labels': string,
+  'intensityUnitsLabel': string,
+  'xRay': string,
+  'ultraviolet': string,
+  'visible': string,
+  'infrared': string
+};
+
+const blackbodySpectrumStrings = getStringModule( 'BLACKBODY_SPECTRUM' ) as StringsType;
+
+blackbodySpectrum.register( 'blackbodySpectrumStrings', blackbodySpectrumStrings );
+
+export default blackbodySpectrumStrings;
