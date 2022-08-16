b2516c5668f2e436106d3c4e4737288bcafaa21f
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:36 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/blackbodySpectrumStrings.ts
+++ js/blackbodySpectrumStrings.ts
@@ -5,32 +5,54 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import blackbodySpectrum from './blackbodySpectrum.js';
 
 type StringsType = {
   'blackbody-spectrum': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'spectralPowerDensityLabel': string;
+  'spectralPowerDensityLabelProperty': TReadOnlyProperty<string>;
   'wavelengthLabel': string;
+  'wavelengthLabelProperty': TReadOnlyProperty<string>;
   'subtitleLabel': string;
+  'subtitleLabelProperty': TReadOnlyProperty<string>;
   'b': string;
+  'bProperty': TReadOnlyProperty<string>;
   'g': string;
+  'gProperty': TReadOnlyProperty<string>;
   'r': string;
+  'rProperty': TReadOnlyProperty<string>;
   'siriusA': string;
+  'siriusAProperty': TReadOnlyProperty<string>;
   'sun': string;
+  'sunProperty': TReadOnlyProperty<string>;
   'lightBulb': string;
+  'lightBulbProperty': TReadOnlyProperty<string>;
   'earth': string;
+  'earthProperty': TReadOnlyProperty<string>;
   'blackbodyTemperature': string;
+  'blackbodyTemperatureProperty': TReadOnlyProperty<string>;
   'kelvinUnits': string;
+  'kelvinUnitsProperty': TReadOnlyProperty<string>;
   'graphValues': string;
+  'graphValuesProperty': TReadOnlyProperty<string>;
   'intensity': string;
+  'intensityProperty': TReadOnlyProperty<string>;
   'labels': string;
+  'labelsProperty': TReadOnlyProperty<string>;
   'intensityUnitsLabel': string;
+  'intensityUnitsLabelProperty': TReadOnlyProperty<string>;
   'xRay': string;
+  'xRayProperty': TReadOnlyProperty<string>;
   'ultraviolet': string;
+  'ultravioletProperty': TReadOnlyProperty<string>;
   'visible': string;
+  'visibleProperty': TReadOnlyProperty<string>;
   'infrared': string;
+  'infraredProperty': TReadOnlyProperty<string>;
 };
 
 const blackbodySpectrumStrings = getStringModule( 'BLACKBODY_SPECTRUM' ) as StringsType;
